556cdf3023b3f9f0e034505370a6f0c25e8ce48c
Raffaele Aquilone <raffaelex.aquilone@intel.com>
2013-08-23 15:18:48 +0200

    [BT] Implement Wide Band Speech configuration
    BZ: 122334
    
    Enabling Wide Band Speech configuration, taking action on two fronts:
     - Implement the function call to the libbt-vendor to toggle the
       Codec and Audio configuration for WBS on the BT chip
     - Raise an event to toggle the WBS Audio configuration into the
       Audio Manager at the Java framework level
    
    Change-Id: I68012b91d86e767f0a3d5e7aa46e8f759be72b07
    Signed-off-by: Raffaele Aquilone <raffaelex.aquilone@intel.com>
    Reviewed-on: http://android.intel.com:8080/119734
    Reviewed-by: Gros, Julien <julien.gros@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- include/bt_target.h
+++ include/bt_target.h
@@ -847,7 +847,7 @@ and USER_HW_DISABLE_API macros */
 
 /* Includes WBS if TRUE */
 #ifndef BTM_WBS_INCLUDED
-#define BTM_WBS_INCLUDED            FALSE       /* TRUE includes WBS code */
+#define BTM_WBS_INCLUDED            TRUE       /* TRUE includes WBS code */
 #endif
 
 /* Includes PCM2 support if TRUE */
