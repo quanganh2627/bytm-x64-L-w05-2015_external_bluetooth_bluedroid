dd080daa7a0fd0d62b682c75e57d3dcddb65ff6a
Cedric Bondier <cedric.bondier@intel.com>
2013-10-09 11:05:49 +0200

    [BT][WCS] WCS solution use another bluedroid
    BZ: 134144
    
    Use compilation flag BOARD_USES_WCS
    
    Change-Id: I3e92dff1dc87b9aa4a72903da125bd90c880ee3f
    Signed-off-by: Cedric Bondier <cedric.bondier@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,7 @@
 ifneq ($(BOARD_HAVE_BLUETOOTH),false)
+
+ifneq ($(BOARD_USES_WCS),true)
+
 LOCAL_PATH := $(call my-dir)
 
 # Setup bdroid local make variables for handling configuration
@@ -15,4 +18,7 @@ include $(call all-subdir-makefiles)
 # Cleanup our locals
 bdroid_C_INCLUDES :=
 bdroid_CFLAGS :=
+
+endif # BOARD_USES_WCS != true
+
 endif
