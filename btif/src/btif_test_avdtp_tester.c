94bb9013364124961257faa060dae25a45dde6ce
Lakshmipathi K <lakshmipathi.k@intel.com>
2014-02-27 12:40:45 +0530

    OSS: R21377 Fix copyright in bluedroid files
    UTP-Id: SMS04859802

--- btif/src/btif_test_avdtp_tester.c
+++ btif/src/btif_test_avdtp_tester.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  *  Copyright (C) 2012-2013 Intel Mobile Communications GmbH
- *
+ *  Copyright (C) 2000-2012 Broadcom Corporation
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at:
