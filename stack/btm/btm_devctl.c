09f0daf12c2a4582a6b9c7e22d67fd59e039085b
Navin Kochar <navin.kochar@intel.com>
2014-09-25 13:44:36 +0530

    [iBT][REVERTME] BT-FM coexistence workaround
    Don't send HCI Reset as a first step of stack init as it causes issues in FM.
    This Workaround will be removed when real fix will be available in FW.
    Issue: MWG100203979
    
    Category: device enablement
    Domain: CWS.BT-intel
    Origin: internal
    Upstream-Candidate: no, proprietary
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-1418
    Change-Id: I433bd7faf47fb70b7b558d6ac11792a980e418c1
    Signed-off-by: Navin Kochar <navin.kochar@intel.com>
    Reviewed-on: https://android.intel.com:443/283020

--- stack/btm/btm_devctl.c
+++ stack/btm/btm_devctl.c
@@ -280,7 +280,11 @@ void BTM_DeviceReset (tBTM_CMPL_CB *p_cb)
         /* Save address of the completion routine, if provided */
         btm_cb.devcb.p_reset_cmpl_cb = p_cb;
 
+#if (INTEL_CONTROLLER == TRUE)
+        btm_reset_complete ();
+#else
         btm_dev_reset ();
+#endif
     }
     else
     {
