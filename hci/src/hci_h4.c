c6043aa51eb64db437302a2142b04516d3da8329
Debnath Paritosh <paritosh.debnath@intel.com>
2014-11-06 18:17:57 +0530

    Fixing memory leak when DEBUG EVENT is received
    Certain commands give Dubug Event which are ignored in
    current implementation. This fix will release the memory
    before ignoring it.
    
    Change-Id: Id09925d1fa590e411d3a8debee91cef39c3f67c9
    Signed-off-by: Paritosh <paritosh.debnath@intel.com>
    Reviewed-on: https://android.intel.com/297795
    Reviewed-by: Rajendran, SadhanaX <sadhanax.rajendran@intel.com>
    Tested-by: Rajendran, SadhanaX <sadhanax.rajendran@intel.com>
    Reviewed-by: Sripathi, Srinivas <srinivas.sripathi@intel.com>
    Reviewed-by: Kandru, Suneel <suneel.kandru@intel.com>

--- hci/src/hci_h4.c
+++ hci/src/hci_h4.c
@@ -331,7 +331,11 @@ uint8_t internal_event_intercept(void)
 
     else if (event_code == HCI_INTEL_DEBUG_EVT)
     {
-        //ignore the event
+        //Ignore the event and release p_rcv_msg buffer
+        if (bt_hc_cbacks)
+        {
+            bt_hc_cbacks->dealloc((TRANSAC) p_cb->p_rcv_msg);
+        }
         return IGNORE_EVENT;
     }
 
